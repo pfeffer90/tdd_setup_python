5229582d60cb2630a9f4d90e88ae25ae6272ea68
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-06-29 12:07:58 +0200

    *introduced a calculator class to make the arrange phases clearer and allow easier extension of the simple example

--- test_role_model.py
+++ test_role_model.py
@@ -1,12 +1,17 @@
-def add(x,y):
-    pass
+class Calculator():
+    @staticmethod
+    def add(x,y):
+        pass
+    def __init__(self):
+        pass
 
-def test_add_adds_two_numbers():
+def test_calculator_adds_two_numbers():
     # Arrange
+    calculator = Calculator()
     first_summand = 2
     second_summand = 3
     expected_sum = 5
     # Act
-    actual_sum = add(first_summand, second_summand)
+    actual_sum = calculator.add(first_summand, second_summand)
     # Assert
     assert expected_sum == actual_sum
