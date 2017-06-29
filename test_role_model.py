9c328d78189b58282fa4f886e7b99301f4c0c76b
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-06-29 11:55:44 +0200

    added unit testing phases

--- test_role_model.py
+++ test_role_model.py
@@ -2,4 +2,11 @@ def add(x,y):
     pass
 
 def test_add_adds_two_numbers():
-    assert add(2,3) == 5
+    # Arrange
+    first_summand = 2
+    second_summand = 3
+    expected_sum = 5
+    # Act
+    actual_sum = add(first_summand, second_summand)
+    # Assert
+    assert expected_sum == actual_sum
